dc3caa1fe2a7a446422d8af5b89010bea6b2f31d
KorneyKSam <korneyksam@gmail.com>
2023-08-19 02:46:50 +0700

    Update Debugger to 2.0.4

--- Assets/GameAssets/Tools/Debugging/DebuggerLog.cs
+++ Assets/GameAssets/Tools/Debugging/DebuggerLog.cs
@@ -4,7 +4,6 @@ namespace Tools.Debugging
     {
         public static string InfoDebug => "InfoDebug";
         public static string InfoWarning => "InfoWarning";
-
         public static string Debug => "Debug";
         public static string Warning => "Warning";
         public static string Error => "Error";
